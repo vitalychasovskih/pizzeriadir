ceebda49d0c83c83d6989e5bd50f5198a9312921
Vitaly Chasovskih <vitalychasovskih@gmail.com>
2023-12-17 16:25:30 +0700

    Добавлен base.html - базовый шаблон, изменен index.html (теперь на основе base.html), добавлены шаблоны для страницы со списком всех пицц и страницы по каждой пицце, изменены урл и вьюхи

--- pizzeria/pizzas/urls.py
+++ pizzeria/pizzas/urls.py
@@ -7,5 +7,9 @@ app_name = 'pizzas'
 
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Page that shows all pizzas.
+    path('pizzas/', views.pizzas_list, name='pizzas_list'),
+    # Detail page for a single pizza with all followed toppings.
+    path('pizzas_list/<int:pizzas_id>/', views.pizza_page, name='pizza_page'),
 ]
--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
+from .models import Pizza
 # Create your views here.
 
 
 def index(request):
     """Home page for pizzas app"""
     return render(request, 'pizzas/index.html')
+
+
+def pizzas_list(request):
+    """Show all pizzas."""
+    pizzas = Pizza.objects.order_by("name")
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas_list.html', context)
+
+
+def pizza_page(request, pizzas_id):
+    """Show a single pizza and all its toppings"""
+    pizza = Pizza.objects.get(id=pizzas_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza_page.html', context)
